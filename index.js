136ec3bd1974c67d3d0061a4901d073c6308c4d4
Mahmud2640 <mdmahmud484@gmail.com>
2022-04-30 11:41:51 +0600

    added some data

--- index.js
+++ index.js
@@ -6,6 +6,27 @@ app.get('/', (req, res) => {
    res.send("Hello world..what's up?!!!! It's working!");
 });
 
+const users = [
+   {id: 1, name: 'John', age: 20, email: 'abc@def.com'},
+   {id: 2, name: 'Kohn', age: 30, email: 'bbc@def.com'},
+   {id: 3, name: 'Lohn', age: 40, email: 'cbc@def.com'},
+   {id: 4, name: 'Mohn', age: 50, email: 'dbc@def.com'},
+   {id: 5, name: 'Nohn', age: 60, email: 'ebc@def.com'},
+   {id: 6, name: 'Oohn', age: 70, email: 'fbc@def.com'},
+   {id: 7, name: 'Pohn', age: 80, email: 'gbc@def.com'},
+]
+
+app.get('/users', (req, res) =>{
+   res.send(users);
+});
+
+app.get('/users/:id', (req, res) =>{
+   console.log(req.params);
+   const id = parseInt(req.params.id);
+   const user = users.find(user => user.id == id);
+   res.send(user);
+});
+
 app.listen(port, () => {
    console.log('Listening to port', port);
 })
